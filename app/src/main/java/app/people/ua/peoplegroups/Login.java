8b772ea71193ab886139a92588122c957245a409
Vladislav Tatara <vladislav23031992@gmail.com>
2018-10-19 01:02:25 +0300

    Start Work! 18.10.2018 Создаем Toolbar(Выдает ошибку при нажатии на кнопки) Убрал с двух активити тулбар, все заработало.

--- app/src/main/java/app/people/ua/peoplegroups/Login.java
+++ app/src/main/java/app/people/ua/peoplegroups/Login.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -28,12 +27,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Регистрация");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         auth = FirebaseAuth.getInstance();
 
         Email_on_login = (EditText)findViewById(R.id.Email_on_login);
--- app/src/main/java/app/people/ua/peoplegroups/Register.java
+++ app/src/main/java/app/people/ua/peoplegroups/Register.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -32,12 +31,6 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Регистрация");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
         final EditText User_name_on_register = (EditText)findViewById(R.id.User_name_on_register);
         final EditText Email_on_register = (EditText)findViewById(R.id.Email_on_register);
