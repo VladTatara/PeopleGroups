e5b561240ef396974d7954ea764c8f6227bdc148
Vladislav Tatara <vladislav23031992@gmail.com>
2018-10-20 20:47:18 +0300

    Start Work! 20.10.2018 Создаем Toolbar(Выдает ошибку при нажатии на кнопки) Убрал с двух активити тулбар, все заработало. Пробую сделать тулбар как в видео уроке

--- app/src/main/java/app/people/ua/peoplegroups/Main.java
+++ app/src/main/java/app/people/ua/peoplegroups/Main.java
@@ -3,23 +3,25 @@ package app.people.ua.peoplegroups;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class Main extends AppCompatActivity {
 
     private Toolbar mToolbar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setTitle("PeopleGroups");
         setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("PeopleGroups");
 
-        mToolbar.setSubtitle("Username");
+    }
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
