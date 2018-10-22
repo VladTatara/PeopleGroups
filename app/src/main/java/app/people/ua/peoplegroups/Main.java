d3c078580221030496eb9798a662615ef74dbb85
Vladislav Tatara <vladislav23031992@gmail.com>
2018-10-23 01:23:07 +0300

    Start Work! 23.10.2018 Создаем Toolbar(Выдает ошибку при нажатии на кнопки) Убрал с двух активити тулбар, все заработало. Пробую сделать тулбар как в видео уроке Остановился на том что выбивает приложение(ошибка на 52 строке в Мейне)

--- app/src/main/java/app/people/ua/peoplegroups/Main.java
+++ app/src/main/java/app/people/ua/peoplegroups/Main.java
@@ -1,27 +1,84 @@
 package app.people.ua.peoplegroups;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Main extends AppCompatActivity {
 
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+    CircleImageView user_image;
+    TextView username;
+
     private Toolbar mToolbar;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar.
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("PeopleGroups");
+        //Определяем по ID елементы.
+        user_image = findViewById(R.id.user_image);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
+        reference.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user_info user  = dataSnapshot.getValue(user_info.class);
+                username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")){
+                    user_image.setImageResource(R.mipmap.ic_launcher);
+                }
+                else {
+                    Glide.with(Main.this).load(user.getImageURL()).into(user_image);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+            });
     }
 
+//Метод для определения меню в верхнем левом углу Toolbar.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+//Метод для обработки элементов меню.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Log_out:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Main.this,Start.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/app/people/ua/peoplegroups/Start.java
+++ app/src/main/java/app/people/ua/peoplegroups/Start.java
@@ -1,8 +1,8 @@
 package app.people.ua.peoplegroups;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,18 +18,23 @@ public class Start extends AppCompatActivity {
     FirebaseUser firebaseUser;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(activity_start);
-        Already_have_account_on_start = (TextView)findViewById(R.id.Already_have_account_on_start);
-        Register_on_start = (Button)findViewById(R.id.Register_on_start);
+    protected void onStart() {
+        super.onStart();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
+        //Проверяем пользователя на наличие в базе!
         if (firebaseUser != null){
             Intent intent = new Intent(Start.this,Main.class);
             startActivity(intent);
             finish();
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_start);
+        Already_have_account_on_start = (TextView)findViewById(R.id.Already_have_account_on_start);
+        Register_on_start = (Button)findViewById(R.id.Register_on_start);
 
         Register_on_start.setOnClickListener(new View.OnClickListener() {
             @Override
