198cbffcdec87eb0c2847ab042cd2d8ce0701f3e
Vladislav Tatara <vladislav23031992@gmail.com>
2018-11-07 00:58:50 +0200

    Start Work!                                   23.10.2018                                   Создаем Toolbar(Выдает ошибку при нажатии на кнопки)                                   Убрал с двух активити тулбар, все заработало.                                   Пробую сделать тулбар как в видео уроке                                   Остановился на том что выбивает приложение(ошибка на 52 строке в Мейне).Убрал ошибку(Айдишники!!!)                                   Исправил ошибку отображения тулбара на вкладе Design                                   Удалил фото пользователя.Добавил функцию восстановления пароля.                                   Поставил Splash.                                   Вставил Navigation.Не реагирует на движение.                                   03.11.2018.Начни с Google Sign-in!                                   Настроил GoogleSignIn                                   Создал Upload_image.                                   Не сработал переход на кнопке ЗАГРУЗИТЬ!ПРОВЕРИТЬ!                                   Сделал рабочим первый пункт в боковом меню!(загрузить).07.11.18

--- app/src/main/java/app/people/ua/peoplegroups/Upload_image.java
+++ app/src/main/java/app/people/ua/peoplegroups/Upload_image.java
@@ -2,12 +2,20 @@ package app.people.ua.peoplegroups;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Upload_image extends AppCompatActivity {
+
+    Button choice_file;
+    EditText enter_name_file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
+
+        choice_file = (Button) findViewById(R.id.choice_file);
+        enter_name_file = (EditText) findViewById(R.id.enter_name_file);
     }
 }
