a9d1368b90aefe56503a1a469224c6c5ac74d6ef
Vladislav Tatara <vladislav23031992@gmail.com>
2018-12-01 01:12:58 +0200

    01.12.18.Сделал иконку приложения и настроил первоначально стрелку НАЗАД на Тулбаре в Галерея. Баг: при возврате через стрелку Тулбара на экране остается Навиг.меню!(так не должно быть)

--- app/src/main/java/app/people/ua/peoplegroups/Galerey.java
+++ app/src/main/java/app/people/ua/peoplegroups/Galerey.java
@@ -1,13 +1,28 @@
 package app.people.ua.peoplegroups;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Galerey extends AppCompatActivity {
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galerey);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
-}
+
+    }
+
