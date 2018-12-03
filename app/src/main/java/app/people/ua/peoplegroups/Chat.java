88a7b756a5b58229b5abbdee7217c130321042d6
Vladislav Tatara <vladislav23031992@gmail.com>
2018-12-04 01:10:25 +0200

    01.12.18.Сделал иконку приложения и настроил первоначально стрелку НАЗАД на Тулбаре в Галерея. Баг: при возврате через стрелку Тулбара на экране остается Навиг.меню!(так не должно быть)Добавил строку ПОЧТА на хейдер...но он не появляется. ПРОВЕРИТЬ. ВСЕ появляется, но при регистрации нового юзера, в Database все есть кроме почты. ПРОВЕРИТЬ Сделал высвечивание в Хейдер ИМЯ и ПОЧТА!НУжно сделать теперь ФОТО пользователя на хейдере и страничку ПРОФИЛЬ ПОЛЬЗОВАТЕЛЯ! 04.12.18. Сделал на вкладке ЧАТ, Фрагменты.

--- app/src/main/java/app/people/ua/peoplegroups/Chat.java
+++ app/src/main/java/app/people/ua/peoplegroups/Chat.java
@@ -1,13 +1,78 @@
 package app.people.ua.peoplegroups;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class Chat extends AppCompatActivity {
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_paper);
+        ViewPaperAdapter viewPaperAdapter = new ViewPaperAdapter(getSupportFragmentManager());
+
+        viewPaperAdapter.addFragment(new ChatFragments(),"Чат" );
+        viewPaperAdapter.addFragment(new UsersFragment(),"Участники" );
+
+        viewPager.setAdapter(viewPaperAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    class ViewPaperAdapter extends FragmentPagerAdapter{
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPaperAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+        public void addFragment(Fragment fragment,String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
