bb37a3fd76994d0927aaeb644383d0e39077781b
Vladislav Tatara <vladislav23031992@gmail.com>
2018-12-06 01:21:59 +0200

    01.12.18.Сделал иконку приложения и настроил первоначально стрелку НАЗАД на Тулбаре в Галерея.                                            Баг: при возврате через стрелку Тулбара на экране остается Навиг.меню!(так не должно быть)Добавил строку ПОЧТА на хейдер...но он не появляется. ПРОВЕРИТЬ.                                            ВСЕ появляется, но при регистрации нового юзера, в Database все есть кроме почты. ПРОВЕРИТЬ                                            Сделал высвечивание в Хейдер ИМЯ и ПОЧТА!НУжно сделать теперь ФОТО пользователя на хейдере и страничку ПРОФИЛЬ ПОЛЬЗОВАТЕЛЯ!                                            04.12.18. Сделал на вкладке ЧАТ, Фрагменты.                                            05.12.18.Сделал Профиль                                            06.12.18.Сделал возможность поставить свою фото на профиль!Повторил как в видеоуроке от KOD Dev

--- app/src/main/java/app/people/ua/peoplegroups/ProfileFragment.java
+++ app/src/main/java/app/people/ua/peoplegroups/ProfileFragment.java
@@ -1,14 +1,24 @@
 package app.people.ua.peoplegroups;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,8 +26,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
@@ -27,6 +45,11 @@ public class ProfileFragment extends Fragment {
     DatabaseReference reference;
     FirebaseUser Puser;
 
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,6 +58,7 @@ public class ProfileFragment extends Fragment {
 
         imageProfile = view.findViewById(R.id.imageProfile);
         username_on_main = view.findViewById(R.id.username_on_main);
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
         Puser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(Puser.getUid());
@@ -58,7 +82,90 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+        imageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
 
         return view;
+    }
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMAGE_REQUEST);
+    }
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+    private void uploadImage(){
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Загрузка");
+        progressDialog.show();
+
+        if (imageUri != null){
+            final StorageReference fileReferense = storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtension(imageUri));
+            uploadTask = fileReferense.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot,Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+if (!task.isSuccessful()){
+    throw task.getException();
+}
+return fileReferense.getDownloadUrl();
+                }
+
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("Users")
+                                .child(Puser.getUid());
+                        HashMap<String,Object>map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        reference.updateChildren(map);
+                        progressDialog.dismiss();
+                    }else {
+                        Toast.makeText(getContext(), "Ошибка", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+
+                }
+            });
+        }else {
+            Toast.makeText(getContext(), "Не выбрано изображение", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(getContext(), "Процесс загрузки", Toast.LENGTH_SHORT).show();
+            }else {
+                uploadImage();
+            }
+        }
     }
 }
