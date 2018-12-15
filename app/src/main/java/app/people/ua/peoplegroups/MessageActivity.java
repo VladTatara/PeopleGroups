33d3e3beca8ef6b7174c4b1beb962dc0932c3fa9
Vladislav Tatara <vladislav23031992@gmail.com>
2018-12-16 00:34:30 +0200

    01.12.18.Сделал иконку приложения и настроил первоначально стрелку НАЗАД на Тулбаре в Галерея.                                            Баг: при возврате через стрелку Тулбара на экране остается Навиг.меню!(так не должно быть)Добавил строку ПОЧТА на хейдер...но он не появляется. ПРОВЕРИТЬ.                                            ВСЕ появляется, но при регистрации нового юзера, в Database все есть кроме почты. ПРОВЕРИТЬ                                            Сделал высвечивание в Хейдер ИМЯ и ПОЧТА!НУжно сделать теперь ФОТО пользователя на хейдере и страничку ПРОФИЛЬ ПОЛЬЗОВАТЕЛЯ!                                            04.12.18. Сделал на вкладке ЧАТ, Фрагменты.                                            05.12.18.Сделал Профиль                                            06.12.18.Сделал возможность поставить свою фото на профиль!Повторил как в видеоуроке от KOD Dev                                            07.12.18.Первая асть настройки странички показа всех пользователей в чате!                                            08.12.18.Сделал страницу зарегестрированных  пользователей!Сделал отправку сообщения пользователю!В базе Data все нормально связано!                                            Донастроить чат между пользователями.                                            11.12.18.Скопировал внешний вид активити для приватного чата! Сообщение так и не выводится на экран!                                            14.12.18Не много подкоректировал логирование. Удалил класс Старт(Он не нужный был).                                            16.12.18Сделал вывод сообщения на экран!(

--- app/src/main/java/app/people/ua/peoplegroups/MessageActivity.java
+++ app/src/main/java/app/people/ua/peoplegroups/MessageActivity.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,11 +20,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import app.people.ua.peoplegroups.adapter.MessageAdapter;
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -34,15 +31,15 @@ public class MessageActivity extends AppCompatActivity {
     CircleImageView imageProfile;
     TextView username_on_main;
     MessageAdapter messageAdapter;
-    List<ChatPerson> vchat;
-
+    List<ChatPerson> vChatPerson;
     RecyclerView recyclerView;
-
     FirebaseUser fuser;
     DatabaseReference reference;
     ImageButton but_send;
     EditText field_for_send_text;
     Intent intent;
+    String userid;
+    boolean notify = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,32 +57,32 @@ public class MessageActivity extends AppCompatActivity {
                 finish();
             }
         });
-
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(linearLayoutManager);
 
-
-
         imageProfile = findViewById(R.id.imageProfile);
         username_on_main = findViewById(R.id.username_on_main);
-        but_send = (ImageButton)findViewById(R.id.but_send);
+        but_send = findViewById(R.id.but_send);
         field_for_send_text = findViewById(R.id.field_for_send_text);
         intent = getIntent();
-        final String userid = intent.getStringExtra("userid");
+        userid = intent.getStringExtra("userid");
+
         fuser = FirebaseAuth.getInstance().getCurrentUser();
         but_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                notify = true;
                 String msg = field_for_send_text.getText().toString();
-                if (msg.equals("")){
+                if (!msg.equals("")){
                    sendMassage(fuser.getUid(),userid ,msg );
                 }
                 else {
                     Toast.makeText(MessageActivity.this, "Вы не можете отправить пустое сообщение!", Toast.LENGTH_SHORT).show();
                 }
+                field_for_send_text.setText("");
             }
         });
 
@@ -100,9 +97,9 @@ public class MessageActivity extends AppCompatActivity {
                     imageProfile.setImageResource(R.drawable.iconmain);
                 }
                 else {
-                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(imageProfile);
+                    Glide.with(getApplicationContext()).load(user.getImageURL()).into(imageProfile);
                 }
-                field_for_send_text.setText("");
+
                 readMessages(fuser.getUid(), userid, user.getImageURL());
 
             }
@@ -114,30 +111,31 @@ public class MessageActivity extends AppCompatActivity {
         });
     }
 
-    private void sendMassage (String sender,String receiver,String message){
+    private void sendMassage (String sender,final String receiver,String message){
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         HashMap<String,Object> hashMap =  new HashMap<>();
         hashMap.put("sender", sender);
-        hashMap.put("recever", receiver);
+        hashMap.put("receiver", receiver);
         hashMap.put("message", message);
-
         reference.child("Chats").push().setValue(hashMap);
 
     }
+
     private void readMessages(final String myid, final String userid, final String imageurl){
-        vchat = new ArrayList<>();
-        reference = FirebaseDatabase.getInstance().getReference("ChatsPrivate");
+        vChatPerson = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                vchat.clear();
+                vChatPerson.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()){
                     ChatPerson chatPerson = snapshot.getValue(ChatPerson.class);
                     if (chatPerson.getReceiver().equals(myid) && chatPerson.getSender().equals(userid) ||
-                            chatPerson.getReceiver().equals(userid)&& chatPerson.getSender().equals(myid)){
-                     vchat.add(chatPerson);
+                            chatPerson.getReceiver().equals(userid) && chatPerson.getSender().equals(myid)){
+                        vChatPerson.add(chatPerson);
                     }
-                   messageAdapter = new MessageAdapter(MessageActivity.this,vchat,imageurl);
+                   messageAdapter = new MessageAdapter(MessageActivity.this,vChatPerson,imageurl);
         recyclerView.setAdapter(messageAdapter);
                 }
             }
